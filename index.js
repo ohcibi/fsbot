c7d61b38e5bda1f09784f61c756e72b294bbbd7e
Tobias Witt <tobias.witt@hhu.de>
2016-10-07 10:09:05 +0200

    Make bot configurable with getconfig

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const irc = require("irc");
+const irc = require("irc")
+const config = require("getconfig")
 
-const client = new irc.Client("irc.quakenet.org", "pen1sp1rat", {
-  channels: ["#hhu-fscs"]
-});
+const client = new irc.Client(config.host, config.nick, {
+  channels: config.channels,
+  showErrors: true,
+  realName: config.realName
+})
 
 client.addListener("message", function(from, to, message) {
-  if (message.substr("moin") && Math.random() > .8) {
-    client.say("#hhu-fscs", `${from}: GUTEN ABEND!!!`);
+  if (message.substr("moin") && Math.random() > 0.8) {
+    broadcast(`${from}: GUTEN ABEND!!!`)
   }
-});
+})
+
+function broadcast(message) {
+  config.channels.forEach(channel => client.say(channel, message))
+}
