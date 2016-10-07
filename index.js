564baa0db080623f648432c5a8f846fcd9165961
Tobias Witt <tobias.witt@hhu.de>
2016-10-07 10:09:30 +0200

    Add simple "!" + query ==> response mechanism

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const irc = require("irc")
 const config = require("getconfig")
 
+const responses = require("./responses")
+
 const client = new irc.Client(config.host, config.nick, {
   channels: config.channels,
   showErrors: true,
@@ -10,6 +12,10 @@ const client = new irc.Client(config.host, config.nick, {
 client.addListener("message", function(from, to, message) {
   if (message.substr("moin") && Math.random() > 0.8) {
     broadcast(`${from}: GUTEN ABEND!!!`)
+  }
+
+  if (message[0] === "!") {
+    broadcast(responses[message.slice(1).split(" ")[0]])
   }
 })
 
